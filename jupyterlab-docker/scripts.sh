2e1671bc5f530342b8690f51aaad54d795b49ff8
Mregojos <86085713+Mregojos@users.noreply.github.com>
2023-04-12 11:45:25 +0800

    Update scripts.sh

--- jupyterlab-docker/scripts.sh
+++ jupyterlab-docker/scripts.sh
@@ -1,7 +1,7 @@
-# Build and Run the Container
+# To build and run the container
 docker build -t jupyterlab .
 cd ..
 docker run --name jupyterlab -p 8888:8888 -v $(pwd):/app jupyterlab 
 
-# TO remove the container
+# To remove the container
 docker rm -f jupyterlab
